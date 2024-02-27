f0c861569e4bd338405ac3918a7b377742c5975b
GauravNardia <codegaurav07@gmail.com>
2024-02-27 14:21:59 +0530

    changed middleware

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,11 +11,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // ERROR HANDLER
-export const handleError = (error: unknown) => {
+export const handleError = async(error: unknown) => {
   if (error instanceof Error) {
     // This is a native JavaScript error (e.g., TypeError, RangeError)
     console.error(error.message);
-    throw new Error(`Error: ${error.message}`);
+     
   } else if (typeof error === "string") {
     // This is a string error message
     console.error(error);
@@ -146,7 +146,7 @@ export const deepMergeObjects = (obj1: any, obj2: any) => {
         obj2[key] &&
         typeof obj2[key] === "object"
       ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+        output[key] = deepMergeObjects(obj1[key], obj2[key]); 
       } else {
         output[key] = obj1[key];
       }
